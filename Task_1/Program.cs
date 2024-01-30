d8408da75808801645c399755fea839244fc8423
zorge <nfursov797@gmail.com>
2024-01-30 15:45:55 +0300

    first commit

---
+++ Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            #region Инициализация переменных
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            #endregion
+
+            int sum = firstNumber + secondNumber;
+            Console.WriteLine("{1} + {2} = {0}", sum, firstNumber, secondNumber);
+            Console.WriteLine("Для завершения нажмите любую клавишу на клавиатуре");
+            Console.ReadKey();
+        }
+    }
+}
